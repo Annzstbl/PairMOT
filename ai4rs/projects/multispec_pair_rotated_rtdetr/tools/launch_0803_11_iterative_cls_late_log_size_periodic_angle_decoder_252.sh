e09efb9dab92d6c7420d3d6fe15815add0277805
lth <tianhaoli1996@gmail.com>
2026-08-03 18:40:40 +0800

    Prepare dual GPU late geometry experiment

---
+++ ai4rs/projects/multispec_pair_rotated_rtdetr/tools/launch_0803_11_iterative_cls_late_log_size_periodic_angle_decoder_252.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+REPO=${PAIRMOT_REPO:-/data/users/litianhao01/PairMmot_lategeom_0803_11_252/ai4rs}
+CONFIG=projects/multispec_pair_rotated_rtdetr/configs/o2_pair_rtdetr_r18vd_2xb4_72e_hsmot_0803_11_iterative_cls_late_log_size_periodic_angle_decoder_252.py
+WORK_DIR=/data4/litianhao/PairMmot/workdir_252/0803_11_paper_base_liquid_encoder_p5temporal_dualevidence_decoder_iterativeclsdnisolatede2e_pairsharedlatelogsizetangent_periodicanglerefinement_pairdn_paircoherent_le180_r18_coco_full_1200x900_bf16_2xb4_fresh
+
+set +u
+source /data/users/litianhao01/anaconda3/etc/profile.d/conda.sh
+conda activate py310
+set -u
+test "${CONDA_DEFAULT_ENV}" = py310
+test "${CONDA_PREFIX}" = /data/users/litianhao01/anaconda3/envs/py310
+PYTHON_ROOT=${CONDA_PREFIX}/bin
+
+trap 'status=$?; echo "[$(date "+%F %T")] formal 0803_11 on 252 failed: status=${status} command=${BASH_COMMAND}" >> "${WORK_DIR}/launch.log"; exit "${status}"' ERR
+
+test ! -e "${WORK_DIR}"
+mkdir -p "${WORK_DIR}"
+cd "${REPO}"
+test -f /data4/litianhao/PairMmot/pretrained_weights/rtdetr_r18vd_dec3_6x_coco_from_paddle_pair_adapted/pair_coco_adapted_pretrain.pth
+test -d /data/users/litianhao01/PairMmot/workdir/aux/gmc_cache/hsmot_train_gap1
+test -d /data/users/litianhao01/PairMmot/workdir/aux/gmc_cache/hsmot_test_gap1
+export CUDA_VISIBLE_DEVICES=${PAIRMOT_CUDA_VISIBLE_DEVICES:-2,3}
+export PYTHONPATH="${REPO}:${PYTHONPATH:-}"
+unset PYTORCH_CUDA_ALLOC_CONF CUBLAS_WORKSPACE_CONFIG TORCH_DISTRIBUTED_DEBUG
+
+echo "[$(date '+%F %T')] fresh formal 0803_11 commit $(git rev-parse --short HEAD)" >> "${WORK_DIR}/launch.log"
+exec "${PYTHON_ROOT}/torchrun" --nproc_per_node=2 --master_port=29858 \
+    tools/train.py "${CONFIG}" --launcher pytorch --work-dir "${WORK_DIR}" \
+    >> "${WORK_DIR}/launch.log" 2>&1
