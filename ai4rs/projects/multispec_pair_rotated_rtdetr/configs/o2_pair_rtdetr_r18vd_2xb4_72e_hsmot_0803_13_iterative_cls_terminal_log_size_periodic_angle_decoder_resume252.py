033ce3ddfd75cd0117d3e03d89bb2f6b79cfb97f
lth <tianhaoli1996@gmail.com>
2026-08-04 04:48:33 +0800

    Prepare mature terminal geometry migration to 252

---
+++ ai4rs/projects/multispec_pair_rotated_rtdetr/configs/o2_pair_rtdetr_r18vd_2xb4_72e_hsmot_0803_13_iterative_cls_terminal_log_size_periodic_angle_decoder_resume252.py
@@ -0,0 +1,48 @@
+"""252-portable continuation of mature 0803_13 from epoch 24.
+
+The scientific model and global batch remain identical to the active 178
+1x8 trajectory. Only physical placement changes to fixed 252 GPU0/1 with
+2x4, allowing the mature branch to continue while 178 explores a successor.
+"""
+from mmengine.config import read_base
+
+with read_base():
+    from .o2_pair_rtdetr_r18vd_2xb4_72e_hsmot_0803_14_iterative_cls_terminal_log_area_periodic_angle_decoder_252 import *  # noqa: F401,F403
+
+
+model['decoder'].update(
+    frame_evidence_cls_decoder=False,
+    frame_detail_cls_decoder=False,
+    pair_shared_shape_refinement_decoder=False,
+    pair_shared_angle_refinement_decoder=False,
+    pair_shared_periodic_angle_refinement_decoder=False,
+    pair_shared_log_size_periodic_angle_refinement_decoder=False,
+    pair_shared_log_area_periodic_angle_refinement_decoder=False,
+    pair_shared_late_log_size_periodic_angle_refinement_decoder=False,
+    pair_shared_terminal_log_size_periodic_angle_refinement_decoder=True,
+    pair_shared_terminal_log_area_periodic_angle_refinement_decoder=False,
+    pair_shared_terminal_periodic_angle_refinement_decoder=False,
+    pair_shared_terminal_normalized_center_refinement_decoder=False,
+    pair_shared_terminal_full_tangent_refinement_decoder=False,
+    pair_shared_terminal_transport_tangent_refinement_decoder=False,
+    pair_shared_progressive_log_shape_periodic_angle_refinement_decoder=False,
+    pair_shared_normalized_center_refinement_decoder=False)
+model['bbox_head'].update(
+    iterative_cls_pair_shared_objectness=False,
+    iterative_cls_terminal_shared_margins=False,
+    iterative_cls_terminal_transport_margins=False,
+    iterative_cls_residual=True,
+    iterative_cls_dn_absolute=True,
+    iterative_cls_detach_between_layers=False)
+
+work_dir = (
+    '/data4/litianhao/PairMmot/workdir_178/'
+    '0803_13_paper_base_liquid_encoder_p5temporal_dualevidence_'
+    'decoder_iterativeclsdnisolatede2e_pairsharedterminallogsizetangent_'
+    'periodicanglerefinement_pairdn_paircoherent_le180_r18_coco_full_'
+    '1200x900_bf16_1xb8_fresh')
+val_evaluator['metrics'].update(
+    track_eval_out_dir=f'{work_dir}/val_track_eval',
+    trackeval_root='/data/users/litianhao01/PairMmot/TrackEval',
+    track_data_root='/data/users/litianhao01/PairMmot/data/hsmot/test')
+test_evaluator = val_evaluator
---
+++ ai4rs/projects/multispec_pair_rotated_rtdetr/tools/resume_0803_13_from_epoch24_terminal_log_size_periodic_angle_decoder_252.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+REPO=${PAIRMOT_REPO:-/data/users/litianhao01/PairMmot_terminal_0803_13_resume252/ai4rs}
+CONFIG=projects/multispec_pair_rotated_rtdetr/configs/o2_pair_rtdetr_r18vd_2xb4_72e_hsmot_0803_13_iterative_cls_terminal_log_size_periodic_angle_decoder_resume252.py
+WORK_DIR=/data4/litianhao/PairMmot/workdir_178/0803_13_paper_base_liquid_encoder_p5temporal_dualevidence_decoder_iterativeclsdnisolatede2e_pairsharedterminallogsizetangent_periodicanglerefinement_pairdn_paircoherent_le180_r18_coco_full_1200x900_bf16_1xb8_fresh
+CHECKPOINT=${WORK_DIR}/epoch_24.pth
+RESUME_LOG=${WORK_DIR}/resume_252_from_epoch24.log
+
+set +u
+source /data/users/litianhao01/anaconda3/etc/profile.d/conda.sh
+conda activate py310
+set -u
+test "${CONDA_DEFAULT_ENV}" = py310
+test "${CONDA_PREFIX}" = /data/users/litianhao01/anaconda3/envs/py310
+PYTHON_ROOT=${CONDA_PREFIX}/bin
+
+trap 'status=$?; echo "[$(date "+%F %T")] resume 0803_13 on 252 failed: status=${status} command=${BASH_COMMAND}" >> "${RESUME_LOG}"; exit "${status}"' ERR
+cd "${REPO}"
+test -d "${WORK_DIR}"
+test -s "${CHECKPOINT}"
+test -f /data4/litianhao/PairMmot/pretrained_weights/rtdetr_r18vd_dec3_6x_coco_from_paddle_pair_adapted/pair_coco_adapted_pretrain.pth
+test -d /data/users/litianhao01/PairMmot/workdir/aux/gmc_cache/hsmot_train_gap1
+test -d /data/users/litianhao01/PairMmot/workdir/aux/gmc_cache/hsmot_test_gap1
+export CUDA_VISIBLE_DEVICES=0,1
+export PYTHONPATH="${REPO}:${PYTHONPATH:-}"
+unset PYTORCH_CUDA_ALLOC_CONF CUBLAS_WORKSPACE_CONFIG TORCH_DISTRIBUTED_DEBUG
+
+echo "[$(date '+%F %T')] resume mature 0803_13 from epoch24 on fixed 252 GPU0,1 commit $(git rev-parse --short HEAD)" >> "${RESUME_LOG}"
+exec "${PYTHON_ROOT}/torchrun" --nproc_per_node=2 --master_port=29871 \
+    tools/train.py "${CONFIG}" --launcher pytorch --work-dir "${WORK_DIR}" \
+    --resume "${CHECKPOINT}" >> "${RESUME_LOG}" 2>&1
