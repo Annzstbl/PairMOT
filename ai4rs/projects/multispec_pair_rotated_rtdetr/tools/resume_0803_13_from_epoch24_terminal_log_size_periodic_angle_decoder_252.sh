3f41d6f4a116fceea51652695827aefef2310b13
lth <tianhaoli1996@gmail.com>
2026-08-04 05:34:25 +0800

    Resume mature decoder in writable 252 workdir

--- ai4rs/projects/multispec_pair_rotated_rtdetr/tools/resume_0803_13_from_epoch24_terminal_log_size_periodic_angle_decoder_252.sh
+++ ai4rs/projects/multispec_pair_rotated_rtdetr/tools/resume_0803_13_from_epoch24_terminal_log_size_periodic_angle_decoder_252.sh
@@ -3,8 +3,9 @@ set -Eeuo pipefail
 
 REPO=${PAIRMOT_REPO:-/data/users/litianhao01/PairMmot_terminal_0803_13_resume252/ai4rs}
 CONFIG=projects/multispec_pair_rotated_rtdetr/configs/o2_pair_rtdetr_r18vd_2xb4_72e_hsmot_0803_13_iterative_cls_terminal_log_size_periodic_angle_decoder_resume252.py
-WORK_DIR=/data4/litianhao/PairMmot/workdir_178/0803_13_paper_base_liquid_encoder_p5temporal_dualevidence_decoder_iterativeclsdnisolatede2e_pairsharedterminallogsizetangent_periodicanglerefinement_pairdn_paircoherent_le180_r18_coco_full_1200x900_bf16_1xb8_fresh
-CHECKPOINT=${WORK_DIR}/epoch_24.pth
+SOURCE_WORK_DIR=/data4/litianhao/PairMmot/workdir_178/0803_13_paper_base_liquid_encoder_p5temporal_dualevidence_decoder_iterativeclsdnisolatede2e_pairsharedterminallogsizetangent_periodicanglerefinement_pairdn_paircoherent_le180_r18_coco_full_1200x900_bf16_1xb8_fresh
+WORK_DIR=/data4/litianhao/PairMmot/workdir_252/0803_13_terminal_log_size_periodic_angle_resume252_from_epoch24
+CHECKPOINT=${SOURCE_WORK_DIR}/epoch_24.pth
 RESUME_LOG=${WORK_DIR}/resume_252_from_epoch24.log
 
 set +u
@@ -15,10 +16,12 @@ test "${CONDA_DEFAULT_ENV}" = py310
 test "${CONDA_PREFIX}" = /data/users/litianhao01/anaconda3/envs/py310
 PYTHON_ROOT=${CONDA_PREFIX}/bin
 
-trap 'status=$?; echo "[$(date "+%F %T")] resume 0803_13 on 252 failed: status=${status} command=${BASH_COMMAND}" >> "${RESUME_LOG}"; exit "${status}"' ERR
 cd "${REPO}"
-test -d "${WORK_DIR}"
+test -d "${SOURCE_WORK_DIR}"
 test -s "${CHECKPOINT}"
+test ! -e "${WORK_DIR}"
+mkdir -p "${WORK_DIR}"
+trap 'status=$?; echo "[$(date "+%F %T")] resume 0803_13 on 252 failed: status=${status} command=${BASH_COMMAND}" >> "${RESUME_LOG}"; exit "${status}"' ERR
 test -f /data4/litianhao/PairMmot/pretrained_weights/rtdetr_r18vd_dec3_6x_coco_from_paddle_pair_adapted/pair_coco_adapted_pretrain.pth
 test -d /data/users/litianhao01/PairMmot/workdir/aux/gmc_cache/hsmot_train_gap1
 test -d /data/users/litianhao01/PairMmot/workdir/aux/gmc_cache/hsmot_test_gap1
