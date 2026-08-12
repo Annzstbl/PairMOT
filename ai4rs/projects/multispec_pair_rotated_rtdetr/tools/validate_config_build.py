12be341f2109fc0e5879f67ba032a28ae7bd085f
lth <tianhaoli1996@gmail.com>
2026-08-12 19:30:43 +0800

    fix: register project modules in config validator

--- ai4rs/projects/multispec_pair_rotated_rtdetr/tools/validate_config_build.py
+++ ai4rs/projects/multispec_pair_rotated_rtdetr/tools/validate_config_build.py
@@ -4,10 +4,17 @@ import sys
 
 from mmengine.config import Config
 from mmengine.runner import Runner
+from mmengine.utils import import_modules_from_strings
+from mmdet.utils import register_all_modules as register_all_modules_mmdet
+from mmrotate.utils import register_all_modules
 
 
 cfg = Config.fromfile(sys.argv[1])
 copy.deepcopy(cfg)
+register_all_modules_mmdet(init_default_scope=False)
+register_all_modules(init_default_scope=False)
+if cfg.get('custom_imports', None):
+    import_modules_from_strings(**cfg.custom_imports)
 runner = Runner.from_cfg(cfg)
 print(
     'READY',
